8c71a9388643d7d6eb8f75a7903094c1521240c1
sufyankhurshid <sufyankhurshid405@gmail.com>
2021-12-24 17:15:07 +0500

    firebase tool install

--- src/utils/firebase/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './authUtils';
-export * from './chatUtils';
--- src/utils/logs.ts
+++
@@ -1,5 +0,0 @@
-export function printLogs(...args: any) {
-  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-    console.log(args);
-  }
-}
