007ebc7d59cee6ef9e4884461cd636fa403a7ca9
heliumRaccoon <helium.raccoon@gmail.com>
2021-12-29 14:49:41 +0500

    admin sdk added

--- src/utils/firebase/authUtils.ts
+++ src/utils/firebase/authUtils.ts
@@ -1,11 +1,32 @@
-import firebase from "firebase/app";
-import { db } from "contexts/FirebaseContext";
+import firebase from 'firebase/app';
+import { db } from 'contexts/FirebaseContext';
 
 export function validateAdmin(uid: string) {
-  const createCode = firebase.functions().httpsCallable("validateAdmin");
+  const createCode = firebase.functions().httpsCallable('validateAdmin');
   return createCode({ uid });
 }
 
 export function getGetTherapist(userId: string) {
-  return db.collection("therapists").where("uid", "==", userId);
+  return db.collection('therapists').where('uid', '==', userId);
 }
+
+const admin = require('firebase-admin');
+const serviceAccount = require('../role-base-system-firebase-adminsdk-kvqdp-59653e7113.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const uid = 'LVz2fW050WY7J9xdviQqBc7u9Qu1';
+const roleBaseClaim = {
+  therapist: true
+};
+admin
+  .auth()
+  .createCustomToken(uid, roleBaseClaim)
+  .then((customToken: any) => {
+    console.log(customToken);
+  })
+  .catch((error: any) => {
+    console.log('Error creating custom token:', error);
+  });
